3d1f4781f8e070a528986c4d7dd695763b9ce483
Sag05 <34776708+Sag05@users.noreply.github.com>
2023-03-18 00:23:24 +0100

    Errror

--- WarThunderAssistant/Form1.Designer.cs
+++ WarThunderAssistant/Form1.Designer.cs
@@ -29,9 +29,8 @@
         private void InitializeComponent()
         {
             this.tasLabel = new System.Windows.Forms.Label();
-            this.debugLabel1 = new System.Windows.Forms.Label();
-            this.debugLabel2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.iasLabel = new System.Windows.Forms.Label();
+            this.fuelLeftLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tasLabel
@@ -42,43 +41,32 @@
             this.tasLabel.Size = new System.Drawing.Size(38, 15);
             this.tasLabel.TabIndex = 0;
             this.tasLabel.Text = "label1";
-            this.tasLabel.Click += new System.EventHandler(this.label1_Click);
             // 
-            // debugLabel1
+            // iasLabel
             // 
-            this.debugLabel1.AutoSize = true;
-            this.debugLabel1.Location = new System.Drawing.Point(12, 33);
-            this.debugLabel1.Name = "debugLabel1";
-            this.debugLabel1.Size = new System.Drawing.Size(38, 15);
-            this.debugLabel1.TabIndex = 1;
-            this.debugLabel1.Text = "label2";
+            this.iasLabel.AutoSize = true;
+            this.iasLabel.Location = new System.Drawing.Point(12, 33);
+            this.iasLabel.Name = "iasLabel";
+            this.iasLabel.Size = new System.Drawing.Size(38, 15);
+            this.iasLabel.TabIndex = 1;
+            this.iasLabel.Text = "label2";
             // 
-            // debugLabel2
+            // fuelLeftLabel
             // 
-            this.debugLabel2.AutoSize = true;
-            this.debugLabel2.Location = new System.Drawing.Point(12, 63);
-            this.debugLabel2.Name = "debugLabel2";
-            this.debugLabel2.Size = new System.Drawing.Size(38, 15);
-            this.debugLabel2.TabIndex = 2;
-            this.debugLabel2.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 91);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.fuelLeftLabel.AutoSize = true;
+            this.fuelLeftLabel.Location = new System.Drawing.Point(12, 63);
+            this.fuelLeftLabel.Name = "fuelLeftLabel";
+            this.fuelLeftLabel.Size = new System.Drawing.Size(38, 15);
+            this.fuelLeftLabel.TabIndex = 2;
+            this.fuelLeftLabel.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.debugLabel2);
-            this.Controls.Add(this.debugLabel1);
+            this.Controls.Add(this.iasLabel);
+            this.Controls.Add(this.fuelLeftLabel);
             this.Controls.Add(this.tasLabel);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -90,8 +78,7 @@
         #endregion
 
         private Label tasLabel;
-        private Label debugLabel1;
-        private Label debugLabel2;
-        private Label label4;
+        private Label iasLabel;
+        private Label fuelLeftLabel;
     }
 }
--- WarThunderAssistant/Form1.cs
+++ WarThunderAssistant/Form1.cs
@@ -24,22 +24,35 @@ namespace WarThunderAssistant
 
         private void HtmlChecker(Object source, ElapsedEventArgs e)
         {
-            //var html = @"http://localhost:8111/";
-            var html = @"C:\Users\samue\Documents\localhost.htm";
+            var html = @"http://localhost:8111/";
+            //var html = @"C:\Users\samue\Documents\localhost.htm";
             HtmlWeb web = new HtmlWeb();
             var htmlDoc = web.Load(html);
 
+            //TAS (True Airspeed)
+            string tas = htmlDoc.GetElementbyId("stt-TAS, km/h").InnerText;
+            tas = tas.Remove(0, 10);
+            tas = "TAS: " + tas + "km/h";
 
-            int tas = Int32.Parse(htmlDoc.GetElementbyId("stt-TAS, km/h").InnerText);
+            //IAS (Indicated Airspeed)
+            string ias = htmlDoc.GetElementbyId("stt-IAS, km/h").InnerText;
+            ias = ias.Remove(0, 10);
+            ias = "IAS: " + ias + "km/h";
+
+            //Current fuel left
+            string fuel = htmlDoc.GetElementbyId("ind-fuel").InnerText.Remove(0, 5).Replace('.', ',');
+            double fuelD = double.Parse(fuel);
+            string fuelConsumption = htmlDoc.GetElementbyId("ind-fuel_consume").InnerText.Remove(0, 13).Replace('.', ',');
+            double fuelConsumptionD = double.Parse(fuelConsumption);
+
+            string fuelLeftS = Math.Round(fuelD / fuelConsumptionD).ToString();
+            string fuelLeft = "Fuel: " + fuelLeftS;
 
             Invoke(new Action(() =>{
-                tasLabel.Text = tas.ToString();
+                tasLabel.Text = tas;
+                iasLabel.Text = ias;
+                fuelLeftLabel.Text = fuelLeft;
             }));
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
